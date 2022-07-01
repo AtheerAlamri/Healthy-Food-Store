32cfa12617de3450db2eab5358930e92b41428c2
Atheer Alamri <Atheeralamri01@gmail.com>
2022-07-01 18:01:34 +0300

    Initial commit

---
+++ Styles/JavaScript.js
@@ -0,0 +1,35 @@
+function HomeFun(){
+  window.location="../Pages/Web.html";
+}
+
+function QFun(){
+    window.location="../Pages/Questions.html";
+  }
+
+  function UsFun(){
+    window.location="../Pages/about_us.html";
+  }
+
+  function ContactFun(){
+    window.location="../Pages/Contact.html";
+  }
+
+  function SnackFun(){
+    window.location="../Pages/Snacks.html";
+  }
+
+
+  function LoginFun(){
+    window.location="../Pages/Login.html";
+  }
+
+
+  function AddFun() {
+	document.querySelector('.bg-modal').style.display = "flex";
+  }
+
+  function Close() {
+	document.getElementById("c").classList.toggle("active");
+}
+
+
